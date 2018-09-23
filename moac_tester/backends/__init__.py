20a9b0e99211f64ac3d6b8dd0c0b9b5815b69980
caizhenglong <caizl2002@hotmail.com>
2018-09-23 08:49:54 +0800

    first commit

--- moac_tester/backends/__init__.py
+++ moac_tester/backends/__init__.py
@@ -9,29 +9,20 @@ from moac_tester.utils.module_loading import (
 from .mock import (   # noqa: F401
     MockBackend,
 )
-from .pyevm import (  # noqa: F401
-    PyEVMBackend,
-    is_pyevm_available,
-)
 
 
 def get_chain_backend_class(backend_import_path=None):
     warnings.simplefilter('default')
 
     if backend_import_path is None:
-        if 'ETHEREUM_TESTER_CHAIN_BACKEND' in os.environ:
-            backend_import_path = os.environ['ETHEREUM_TESTER_CHAIN_BACKEND']
-        elif is_pyevm_available():
-            vi = sys.version_info
-            if vi.major != 3 or vi.minor < 5:
-                warnings.warn(UserWarning("Py-EVM does not support python < 3.5"))
-            backend_import_path = get_import_path(PyEVMBackend)
+        if 'MOAC_TESTER_CHAIN_BACKEND' in os.environ:
+            backend_import_path = os.environ['MOAC_TESTER_CHAIN_BACKEND']
         else:
             warnings.warn(UserWarning(
-                "Ethereum Tester: No backend was explicitely set, and no *full* "
+                "Moac Tester: No backend was explicitely set, and no *full* "
                 "backends were available.  Falling back to the `MockBackend` "
                 "which does not support all EVM functionality.  Please refer to "
-                "the `eth-tester` documentation for information on what "
+                "the `moac-tester` documentation for information on what "
                 "backends are available and how to set them."
             ))
             backend_import_path = get_import_path(MockBackend)
